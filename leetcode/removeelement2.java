b60aa172e7f7cf6fbcd6fa813b596a8b6c80af7e
Lavi sharma <lavipandit123@gmail.com>
2024-12-31 00:34:31 +0530

    submitted first leetcode problem with return as k

--- leetcode/removeelement2.java
+++ leetcode/removeelement2.java
@@ -4,27 +4,19 @@ import java.util.Arrays;
 
 public class removeelement2 {
     public static void main(String[] args) {
-        int nums[] = {3, 2, 2, 3,};
+        int nums[] = {0,1,2,2,3,0,4,2,};
         int val = 2;
-        removeElement(nums, val);
+        System.out.println(removeElement(nums, val));
     }
 
-    static void removeElement(int[] nums, int val) {
-        int k = nums.length;
+    static int removeElement(int[] nums, int val) {
+        int j =0;
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == val) {
-                for (int j = i + 1; j < nums.length - 1; j++) {
-                    if ((nums[j] != val)&&(nums[i] == val)) {
-                        int temp = nums[j];
-                        nums[j] = nums[i];
-                        nums[i] = temp;
-                        k = k- 1;
-                    }
-                }
+            if(nums[i]!=val){
+                nums[j++] = nums[i];
             }
         }
-        System.out.println(k);
-        System.out.println(Arrays.toString(nums));
+        return j;
     }
 }
 
