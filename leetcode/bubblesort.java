3e48de812d1e18c589542ce1cc9ee21622f5e28a
Lavi sharma <lavipandit123@gmail.com>
2025-01-02 23:26:29 +0530

    added bubble sort

---
+++ leetcode/bubblesort.java
@@ -0,0 +1,7 @@
+package leetcode;
+
+public class bubblesort {
+    public static void main(String[] args) {
+        int arr[] = {32,33,53,53,65,32,13};
+    }
+}
---
+++ leetcode/problema.java
@@ -0,0 +1,7 @@
+package leetcode;
+
+public class problema {
+    public static void main(String[] args) {
+        int arr[] = {23,33 ,43};
+    }
+}
