5e61e0121332afff998f87661256224ec8662caf
Lavi sharma <lavipandit123@gmail.com>
2025-01-07 23:28:59 +0530

    added transfromed array

--- leetcode/tranformedarray.java
+++ leetcode/tranformedarray.java
@@ -9,20 +9,21 @@ public class tranformedarray {
         System.out.println(Arrays.toString(array));
     }
     static int[] tranformarray(int[] nums){
-        int i,j;
+        int i;
         int[] result = new int[nums.length];
         int n = nums.length;
         for (i=0;i<nums.length;i++){
-            j = i + nums[i];
-            if(0<=j && j<nums.length){
+            if(nums[i]>0){
+                int j = (i + nums[i])%n;
                 result[i] = nums[j];
-            }
-            else if(j>=nums.length){
-                j = j-nums.length;
+            } else if(nums[i] < 0) {
+                int j = (i+ nums[i])%n;
+                if (j<0){
+                    j = n+j;
+                }
                 result[i] = nums[j];
-            }else if(j<0){
-                j = nums.length + j;
-                result[i] = nums[j];
+            } else {
+                result[i] = nums[i];
             }
         }
         return result;
