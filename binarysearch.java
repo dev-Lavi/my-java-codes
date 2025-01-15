fbdeb906443d18c66f5795e516e7724d793d7fae
Lavi sharma <lavipandit123@gmail.com>
2025-01-15 23:25:37 +0530

    added binary search

---
+++ binarysearch.java
@@ -0,0 +1,10 @@
+public class binarysearch {
+    public static void main(String[] args) {
+        int[] arr = {1,4,6,7,8,23,54,65,76,};
+        binaryseacrh(arr);
+    }
+
+    static int[] binaryseacrh(int[] arr){
+        return arr;
+    }
+}
