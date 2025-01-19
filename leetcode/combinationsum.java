e0747d5d5f5df8013e9a8c335a37ebe784d47e1b
Lavi sharma <lavipandit123@gmail.com>
2025-01-19 23:21:39 +0530

    added combination sum

--- leetcode/combinationsum.java
+++ leetcode/combinationsum.java
@@ -1,6 +1,12 @@
 package leetcode;
 
 public class combinationsum {
-    int[] arr = {1,43,235,34,12};
-    int target = 65;
+    public static void main(String[] args) {
+        int[] arr = {1,43,235,34,12};
+        int target = 65;
+    }
+
+    static int[] combinationsum(int[] arr, int target){
+        return arr;
+    }
 }
