305348690e8fd783497b22288366c25c40ea3aa4
Lavi sharma <lavipandit123@gmail.com>
2025-02-03 00:23:54 +0530

    added kth elemnt

--- leetcode/kthsmallest.java
+++ leetcode/kthsmallest.java
@@ -4,24 +4,38 @@ public class kthsmallest {
     public static void main(String[] args) {
         int[][] matrix = {{1,5,9},{10,11,13},{12,13,15}};
         int k = 8;
-        kthsmallest(matrix,k);
+        int answer = kthsmallest(matrix,k);
+        System.out.println(answer);
     }
     static int kthsmallest(int[][] matrix,int k){
         int n = matrix.length;
         int low = matrix[0][0];
         int high = matrix[n-1][n-1];
-        while(low<=high){
+
+        while(low != high){
             int mid = low + (high - low)/2;
-            int count = 0;
-            for(int i=0; i<n; i++){
-                 count = count + upper_bound(matrix[i].begin(),matrix[i].end(),mid) - matrix[i].begin();
-            }
+            int count = numberoflessorequalthan( matrix, mid);
             if(count<k){
                 low = mid + 1;
             }else{
                 high = mid;
             }
         }
-        return low;
+        return high;
+    }
+
+    public static int numberoflessorequalthan(int[][] matrix,int mid){
+        int count = 0;
+        int row = 0;
+        int column = matrix.length-1;
+        while(row<matrix.length && column>=0){
+            if(matrix[row][column]<=mid){
+                count = count + column + 1;
+                row++;
+            }else{
+                column--;
+            }
+        }
+        return count;
     }
 }
