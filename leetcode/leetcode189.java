66f2d52281b57d6fac2ed9d711e0eed0d6f74d0e
Lavi sharma <lavipandit123@gmail.com>
2025-03-02 01:52:27 +0530

    added binary search for sorted 2darray

---
+++ leetcode/leetcode189.java
@@ -0,0 +1,34 @@
+package leetcode;
+
+public class leetcode189 {
+    public static void main(String[] args) {
+        int[] nums = {-1,-100,3,99};
+        int k = 2;
+        rotate(nums, k);
+    }
+
+    static void rotate(int[] nums, int k){
+        int n = nums.length;
+        while(k>0){
+            int t=0;
+            int s=0;
+            for(int i=0; i<n; i++){
+                if( i == n-1 ){
+                    nums[0] = s;
+                }else if(i==0){
+                    t = nums[i+1];
+                    nums[i+1]=nums[i];
+                }else{
+                    if( i%2 == 0 ){
+                        t = nums[i+1];
+                        nums[i+1] = s;
+                    }else{
+                        s = nums[i+1];
+                        nums[i+1] = t;
+                    }
+                }
+            }
+            k--;
+        }
+    }
+}
